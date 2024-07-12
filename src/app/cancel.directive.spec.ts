09d4260ba04cd2aff543bc17846fec88be8d27fd
kristianfiliposki <142878489+kristianfiliposki@users.noreply.github.com>
2024-07-12 14:35:34 +0200

    Form done

---
+++ src/app/cancel.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CancelDirective } from './cancel.directive';
+
+describe('CancelDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CancelDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/cancel.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[cancel]',
+  standalone: true
+})
+export class CancelDirective {
+
+  constructor() { }
+
+}
